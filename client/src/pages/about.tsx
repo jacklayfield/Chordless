34e537b31078f2d061b9b191066c742606cef4a7
jacklayfield <layf100@gmail.com>
2023-01-10 23:42:00 -0500

    Updated about page

--- client/src/pages/about.tsx
+++ client/src/pages/about.tsx
@@ -23,41 +23,60 @@ export const About = () => {
                   <img className="sectionImg" src={guitar_player} alt="" />{" "}
                   <br />
                   <br />
-                  <span>Chordless has two goals:</span> <br /> <br />
+                  <span>Chordless has three goals:</span> <br /> <br />
                   <span style={{ fontWeight: "bold" }}>
                     1. To allow users to create songs without needing to know
-                    every chord or shape they are playing
+                    every chord or note(s) they are playing.
                   </span>{" "}
                   <br />
                   <span>
                     As a beginner guitarist I found that it difficult to write
-                    songs that weren't composed of the basic chords I had
-                    learned and was used to playing. Whenever I was
-                    experimenting and playing different shapes and patterns
-                    across the fretboard I would find it hard to be able to
-                    easily record what I was playing. Even now as a more
-                    experienced guitar player I still admit I find myself
-                    struggling to know certain less common shapes and it is a
-                    pain to have to look them up and write them down. Therefore,
-                    I have created this app to allow users to simply input the
-                    exact strings and frets they've played, and it will
-                    automatically save that shape and it's name for future use.
+                    songs that weren't composed of the basic chords I was used
+                    to playing. Whenever I was experimenting and playing
+                    different shapes and patterns across the fretboard I would
+                    find it to be a huge pain to go from playing to looking up
+                    the chord and recording it. Even now as a more experienced
+                    guitar player I still admit I find myself struggling to know
+                    certain less common shapes and it is certainly inconvenient
+                    to have to figure out / look them up. Therefore, I have
+                    created this app to allow users to simply input the shape
+                    they've played, and it will automatically save that shape
+                    complete with its generated chord name / note for future
+                    use.
                   </span>{" "}
                   <br />
                   <br />
                   <span style={{ fontWeight: "bold" }}>
-                    2. To create a place to write and save songs
+                    2. To create a proper place to write down and keep track of
+                    riffs / progressions on the fly
                   </span>{" "}
                   <br />
                   <span>
-                    There are not a lot of good free options out there desinged
-                    to help guitarists write and save songs. After talking with
-                    other guitarists I found they echoed my same desire for a
-                    simple tool to write and store songs that can not only be
-                    easily accessed but also updated when so desired. I have
-                    designed this app with the intention of providing an easy
-                    and clean way of creating, updating, and viewing songs to
-                    fit this need.
+                    While awesome options exist for comprehensive songwriting,
+                    when it comes to small riffs / progressions it becomes clear
+                    these products are the wrong tool for the job. They bombard
+                    you with set up and features you don't need while
+                    underdelivering on features that would prove helpful in the
+                    process. This is not so much of a songwriting app as it is a
+                    sophisticated "note-taking" app for guitarists. Just played
+                    a riff you like? Effortlessly save and refer to it without
+                    having to go through the hassle of creating an entire
+                    comprehensive song.
+                  </span>{" "}
+                  <br />
+                  <br />
+                  <span style={{ fontWeight: "bold" }}>
+                    3. To utilize the "shape" display as opposed to only the
+                    chord names or tabs.
+                  </span>{" "}
+                  <br />
+                  <span>
+                    Looking at tabs can make learning new songs slow and
+                    daunting, especially for begginers. Sometimes breaking
+                    things out into steps and showing you exactly where to put
+                    your fingers on what looks like your actual fretboard makes
+                    the process a lot less frustrating. That's why this view is
+                    made available (alongside the others).{" "}
                   </span>{" "}
                 </div>
               </Row>
