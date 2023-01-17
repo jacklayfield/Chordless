58fe67a81e53691735b0024e29848adada23afd8
jacklayfield <layf100@gmail.com>
2023-01-16 23:35:40 -0500

    Adding login styling

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,21 +1,23 @@
 import Google from "../google.png";
 import "../styling/login.css";
+import "../styling/theme.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export const Login = () => {
   const google = () => {
     window.open("http://localhost:8000/auth/google", "_self");
   };
   return (
-    <div>
-      <h1>Choose a Login Method</h1>
-
-      <div className="loginButton google" onClick={google}>
-        Google
-      </div>
-      <div>
-        <input type="text" placeholder="Username" />
-        <input type="text" placeholder="Password" />
-        <button>Login</button>
+    <div className="login">
+      <div className="wrapper">
+        <div className="center">
+          <h1 className="loginTitle">Login with Google</h1>
+          <div className="loginButton google" onClick={google}>
+            <img src={Google} alt="" className="icon" />
+            Google
+          </div>
+        </div>
       </div>
     </div>
   );
