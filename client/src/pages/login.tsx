532583690a0d67498ca93290e294209c61cdd920
jacklayfield <layf100@gmail.com>
2023-01-17 23:54:05 -0500

    Improved login styling

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,19 +1,28 @@
 import Google from "../google.png";
 import "../styling/login.css";
 import "../styling/theme.css";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 
 export const Login = () => {
   const google = () => {
     window.open("http://localhost:8000/auth/google", "_self");
   };
   return (
-    <div className="login">
-      <div className="wrapper">
-        <div className="center">
-          <h1 className="loginTitle">Login with Google</h1>
-          <div className="loginButton google" onClick={google}>
+    <div className="center">
+      <div className="cover">
+        <h1 className="login-text">Login</h1>
+        <div className="input-container">
+          <input type="text" placeholder="username" />
+        </div>
+        <div className="input-container bottom">
+          <input type="password" placeholder="password" />
+        </div>
+
+        <div className="login-btn">Login</div>
+
+        <h4 className="alt-login-text">Or login with</h4>
+
+        <div className="alt-login">
+          <div className="login-btn google" onClick={google}>
             <img src={Google} alt="" className="icon" />
             Google
           </div>
