b18ee00be4147c377db9a504aa259dd140e314bd
jacklayfield <layf100@gmail.com>
2023-10-25 20:13:39 -0400

    Dark mode persistence across pages

--- client/src/components/general/modeSwitch.tsx
+++ client/src/components/general/modeSwitch.tsx
@@ -1,25 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const ModeSwitch = () => {
-  const [checked, setChecked] = useState<boolean>(false);
-
-  const setMode = () => {
-    if (
-      document.querySelector("body")?.getAttribute("data-theme") === "light" ||
-      document.querySelector("body")?.getAttribute("data-theme") === null
-    ) {
-      document.querySelector("body")?.setAttribute("data-theme", "dark");
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
     } else {
-      document.querySelector("body")?.setAttribute("data-theme", "light");
+      setTheme("light");
     }
-    setChecked(!checked);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    document.querySelector("body")?.setAttribute("data-theme", theme);
+
+    // Force dark slider to stay on right side between page changes
+    if (theme === "dark") {
+      document.querySelector("input")?.setAttribute("checked", "checked");
+    } else {
+      document.querySelector("input")?.removeAttribute("checked");
+    }
+  }, [theme]);
+
+  const madness = () => {
+    setTheme("madness");
+    document.querySelector("input")?.removeAttribute("checked");
   };
 
   return (
     <div className="center-div pt-4">
-      <label>Dark / Light Mode</label>
+      <label onClick={madness}>Toggle Mode ({theme})</label>
       <label className="switch">
-        <input type="checkbox" onClick={setMode} />
+        <input type="checkbox" id="switch" onClick={toggleTheme} />
         <span className="slider" />
       </label>
     </div>
