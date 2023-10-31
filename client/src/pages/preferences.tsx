2be0498f2d981a89a261a3f2df6926eef2f201b9
jacklayfield <layf100@gmail.com>
2023-10-30 22:29:25 -0400

    Preferences frontend completed

--- client/src/pages/preferences.tsx
+++ client/src/pages/preferences.tsx
@@ -10,13 +10,9 @@ export const Preferences = () => {
     React.useContext(CurrentUserContext);
 
   useEffect(() => {
-    const fetchUserDetails = () => {
-      
-    };
+    const fetchUserDetails = () => {};
     fetchUserDetails();
   }, [authIsLoading]);
-
-
 
   if (authIsLoading) {
     return <Loading />;
@@ -27,15 +23,82 @@ export const Preferences = () => {
       <>
         <ToastContainer autoClose={2000} />
         <div className="inner-div">
-
-          {/**sections: 
+          {/**sections:
            * Dark/Light mode
            * guitar color / type
            * guitar sound
-           * 
-          */}
+           *
+           */}
 
-          
+          <div className="preference-item">
+            <label className="preference-item-title">Guitar Sound</label>
+            <label className="radio-inline">
+              <input
+                type="radio"
+                value="Acoustic"
+                name="sound"
+                className="settings-radio"
+              />{" "}
+              Acoustic
+            </label>
+            <label className="radio-inline">
+              <input
+                type="radio"
+                value="Piano"
+                name="sound"
+                className="settings-radio"
+              />{" "}
+              Piano
+            </label>
+            <label className="radio-inline">
+              <input
+                type="radio"
+                value="Organ"
+                name="sound"
+                className="settings-radio"
+              />{" "}
+              Organ
+            </label>
+            <label className="radio-inline">
+              <input
+                type="radio"
+                value="EDM"
+                name="sound"
+                className="settings-radio"
+              />{" "}
+              EDM
+            </label>
+          </div>
+          <div className="preference-item mt-3">
+            <label className="preference-item-title">Guitar Appearance</label>
+            <label className="radio-inline">
+              <input
+                type="radio"
+                value="Classic"
+                name="appearance"
+                className="settings-radio"
+              />{" "}
+              Classic
+            </label>
+            <label className="radio-inline">
+              <input
+                type="radio"
+                value="Light"
+                name="appearance"
+                className="settings-radio"
+              />{" "}
+              Light
+            </label>
+            <label className="radio-inline">
+              <input
+                type="radio"
+                value="Fancy"
+                name="appearance"
+                className="settings-radio"
+              />{" "}
+              Fancy
+            </label>
+          </div>
         </div>
       </>
     );
