9a0dd0dc9c8cdc91ef99d98c5c8a2377e91a1480
jacklayfield <layf100@gmail.com>
2023-11-01 21:59:55 -0400

    Preferences checked on load

--- client/src/pages/preferences.tsx
+++ client/src/pages/preferences.tsx
@@ -9,15 +9,41 @@ export const Preferences = () => {
   const { currentUser, authIsLoading, handleLogout } =
     React.useContext(CurrentUserContext);
   const [userPreferences, setUserPreferences] = useState<string[]>([]);
+  const [madeEdit, setMadeEdit] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchUserDetails = () => {
       if (!authIsLoading && currentUser?.preferences != null) {
         setUserPreferences(currentUser.preferences);
+
+        const soundElem = document.getElementById(
+          currentUser.preferences[0]
+        ) as HTMLInputElement;
+        const appearanceElem = document.getElementById(
+          currentUser.preferences[1]
+        ) as HTMLInputElement;
+        if (soundElem != null) {
+          soundElem.checked = true;
+        }
+        if (appearanceElem != null) {
+          appearanceElem.checked = true;
+        }
       }
     };
     fetchUserDetails();
   }, [authIsLoading, currentUser?.preferences]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value);
+    setMadeEdit(true);
+  };
+
+  const handleSave = () => {
+    // Save the change
+    // Either A. Force Refresh, B. setMadeEdit(false)
+    // I like A because it gives the user feedback that an action has been taken
+    // THough for B could just send a notification that update was made
+  };
 
   if (authIsLoading) {
     return <Loading />;
@@ -28,19 +54,11 @@ export const Preferences = () => {
       <>
         <ToastContainer autoClose={2000} />
         <div className="inner-div">
-          {/**sections:
-           * Dark/Light mode
-           * guitar color / type
-           * guitar sound
-           *
-           */}
-
-          {userPreferences}
-
-          <div className="preference-item">
+          <div className="preference-item" onChange={handleChange}>
             <label className="preference-item-title">Guitar Sound</label>
             <label className="radio-inline">
               <input
+                id="Acoustic"
                 type="radio"
                 value="Acoustic"
                 name="sound"
@@ -50,6 +68,7 @@ export const Preferences = () => {
             </label>
             <label className="radio-inline">
               <input
+                id="Piano"
                 type="radio"
                 value="Piano"
                 name="sound"
@@ -59,6 +78,7 @@ export const Preferences = () => {
             </label>
             <label className="radio-inline">
               <input
+                id="Organ"
                 type="radio"
                 value="Organ"
                 name="sound"
@@ -68,6 +88,7 @@ export const Preferences = () => {
             </label>
             <label className="radio-inline">
               <input
+                id="EDM"
                 type="radio"
                 value="EDM"
                 name="sound"
@@ -80,6 +101,7 @@ export const Preferences = () => {
             <label className="preference-item-title">Guitar Appearance</label>
             <label className="radio-inline">
               <input
+                id="Classic"
                 type="radio"
                 value="Classic"
                 name="appearance"
@@ -89,6 +111,7 @@ export const Preferences = () => {
             </label>
             <label className="radio-inline">
               <input
+                id="Light"
                 type="radio"
                 value="Light"
                 name="appearance"
@@ -98,6 +121,7 @@ export const Preferences = () => {
             </label>
             <label className="radio-inline">
               <input
+                id="Fancy"
                 type="radio"
                 value="Fancy"
                 name="appearance"
@@ -106,6 +130,16 @@ export const Preferences = () => {
               Fancy
             </label>
           </div>
+          {madeEdit && (
+            <div className="flex-container center p-0">
+              <div className="chordless-btn edit-chords mt-4">
+                <div className="song-options-save">
+                  {" "}
+                  <i className="fa-solid fa-floppy-disk fa-lg"></i> Save Changes
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </>
     );
